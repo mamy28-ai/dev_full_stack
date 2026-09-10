2c2123f603d86a0bd5bfa56561072b6f76bd601a
Angelica <hajatianandriamamonjy@gmail.com>
2026-09-10 10:47:59 +0300

    backAppModifier

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ import profsRoutes from "./routes/profs.routes.js";
 import elevesRoutes from "./routes/eleves.routes.js";
 import classesRoutes from "./routes/classes.routes.js";
 import usersRoutes from "./routes/users.routes.js";
+import enseignantsRoutes from "./routes/enseignants.rotutes.js";
+import noteRoutes from "./routes/note.routes.js";
+import matiereRoutes from "./routes/matiere.route.js"
 
 const app = express();
 
@@ -15,5 +18,8 @@ app.use("/api/profs", profsRoutes);
 app.use("/api/eleves", elevesRoutes);
 app.use("/api/classes", classesRoutes);
 app.use("/api/user", usersRoutes);
+app.use("/api/enseignants" , enseignantsRoutes);
+app.use("/api/note", noteRoutes);
+app.use("/api/matiere", matiereRoutes);
 
 export default app;
